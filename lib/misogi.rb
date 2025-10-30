dafacca4e5474a9a6cfd6e2def761fc69ca40703
iyuuya <yuya.ito@kufu.co.jp>
2025-10-31 08:45:11 +0900

    autoload

--- lib/misogi.rb
+++ lib/misogi.rb
@@ -1,18 +1,27 @@
 # frozen_string_literal: true
 
+# 基本的な型定義は即座にロード
 require_relative "misogi/version"
 require_relative "misogi/violation"
 require_relative "misogi/parsed_content"
-require_relative "misogi/parser/base"
-require_relative "misogi/parser/ruby"
-require_relative "misogi/rule/base"
-require_relative "misogi/rule/ruby_standard"
-require_relative "misogi/rule/rails"
-require_relative "misogi/rule/rspec"
-require_relative "misogi/validator"
-require_relative "misogi/configuration"
-require_relative "misogi/cli"
 
 module Misogi
   class Error < StandardError; end
+
+  # 遅延ロードするクラス/モジュールをautoloadで定義
+  autoload :CLI, "misogi/cli"
+  autoload :Configuration, "misogi/configuration"
+  autoload :Validator, "misogi/validator"
+
+  module Parser
+    autoload :Base, "misogi/parser/base"
+    autoload :Ruby, "misogi/parser/ruby"
+  end
+
+  module Rule
+    autoload :Base, "misogi/rule/base"
+    autoload :RubyStandard, "misogi/rule/ruby_standard"
+    autoload :Rails, "misogi/rule/rails"
+    autoload :RSpec, "misogi/rule/rspec"
+  end
 end
