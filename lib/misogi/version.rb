0e67ef2a5f4d7e1de7c95db28bcac8b228d3bcce
iyuuya <yuya.ito@kufu.co.jp>
2025-10-30 09:29:37 +0900

    bundle gem misogi

---
+++ lib/misogi/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Misogi
+  VERSION = "0.1.0"
+end
---
+++ spec/misogi_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Misogi do
+  it "has a version number" do
+    expect(Misogi::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
