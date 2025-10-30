d4906436517750a242aec3ed4c0966080e4fd0f6
iyuuya <yuya.ito@kufu.co.jp>
2025-10-31 08:49:34 +0900

    モジュールドキュメント追加

--- lib/misogi.rb
+++ lib/misogi.rb
@@ -5,6 +5,7 @@ require_relative "misogi/version"
 require_relative "misogi/violation"
 require_relative "misogi/parsed_content"
 
+# Misogiはファイルの内容を解析して、ファイル名やディレクトリ配置が適切かをチェックするlintツールを提供します
 module Misogi
   class Error < StandardError; end
 
@@ -13,11 +14,13 @@ module Misogi
   autoload :Configuration, "misogi/configuration"
   autoload :Validator, "misogi/validator"
 
+  # ファイルの内容を解析するパーサー
   module Parser
     autoload :Base, "misogi/parser/base"
     autoload :Ruby, "misogi/parser/ruby"
   end
 
+  # ファイルパスとコード内容の整合性をチェックするルール
   module Rule
     autoload :Base, "misogi/rule/base"
     autoload :RubyStandard, "misogi/rule/ruby_standard"
